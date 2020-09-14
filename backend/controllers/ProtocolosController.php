0f5b600041d9845d9572b9e69bb12cce4b111e32
AlendreLopes <alendrelopes@gmail.com>
2020-09-14 15:21:33 -0300

    Revert "Reports - Alterado o layout de cadastro protocolo"
    This reverts commit ea7aedb196261b771652cdabe7aa3530af449b10.

--- backend/controllers/ProtocolosController.php
+++ backend/controllers/ProtocolosController.php
@@ -147,7 +147,6 @@ class ProtocolosController extends AppController
      */
     public function actionCreateReport($id)
     {
-        $this->layout = "mainRegister";
         $model = $this->findModel($id);
         $cookies = Yii::$app->response->cookies;
         $cookies->add(new \yii\web\Cookie([
