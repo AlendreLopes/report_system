933a111144b37d42596800863cebaec4b2dc064a
AlendreLopes <alendrelopes@gmail.com>
2020-09-25 02:01:31 -0300

    Reports - Protocolocos Id inferior a 61134 + 600 atualizado o hash

--- backend/controllers/ProtocolosController.php
+++ backend/controllers/ProtocolosController.php
@@ -23,15 +23,16 @@ class ProtocolosController extends AppController
      */
     public function actionIndex()
     {
-        \set_time_limit(360);
+        /*\set_time_limit(360);
         $numero = 0;
         $query = (new \yii\db\Query())
         ->from('protocolos')
         ->select(['id','motedepass'])
-        ->where(['<','id','61135'])        
+        ->where(['<','id','61135']) 
         ->orderBy(['id' => SORT_DESC,])
         ->indexBy('id');
         foreach ($query->each() as $key) {
+
             $protocolo = Protocolos::findOne($key['id']);
             $protocolo->scenario = 'passha';
             $protocolo->password_hash = Yii::$app->security->generatePasswordHash($key['motedepass']); 
@@ -43,6 +44,7 @@ class ProtocolosController extends AppController
         }
         echo "Contador: ".$numero;
         die();
+        */
         $cookies = Yii::$app->request->cookies;
         if (isset($cookies['protocolos_id'])) {
             $cookies = Yii::$app->response->cookies;
