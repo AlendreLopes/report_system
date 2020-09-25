063ecf649a8aa8d464a463f086c09f507b25c31b
AlendreLopes <alendrelopes@gmail.com>
2020-09-25 01:50:14 -0300

    Reports - Protocolos atualizando hash id 61184

--- backend/controllers/ProtocolosController.php
+++ backend/controllers/ProtocolosController.php
@@ -23,13 +23,12 @@ class ProtocolosController extends AppController
      */
     public function actionIndex()
     {
-        /*
         \set_time_limit(360);
         $numero = 0;
         $query = (new \yii\db\Query())
         ->from('protocolos')
         ->select(['id','motedepass'])
-        ->where(['<','id','61'])        
+        ->where(['=','id','61184'])        
         ->orderBy(['id' => SORT_DESC,])
         ->indexBy('id');
         foreach ($query->each() as $key) {
@@ -44,7 +43,6 @@ class ProtocolosController extends AppController
         }
         echo "Contador: ".$numero;
         die();
-        */
         $cookies = Yii::$app->request->cookies;
         if (isset($cookies['protocolos_id'])) {
             $cookies = Yii::$app->response->cookies;
